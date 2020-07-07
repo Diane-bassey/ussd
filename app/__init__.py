ecc81a88078ab8e3c55365409e38e632ba40fbb1
bassey diane <dianebasseyprincess@gmail.com>
2020-07-07 13:51:11 +0100

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+# from flask_pymongo import PyMongo
+import africastalking
+
+
+app = Flask(__name__)
+
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from app import app
+from flask import make_response, request
+
+@app.route("/", methods = ["POST", "GET"])
+def index():
+    response = make_response["END connection ok"]
+    response.headers['Content-Type'] ="text/plain"
+    return response
+
+
+@app.route("/ussdres", methods = ["POST", "GET"])
+def res():
+    session_id = request.values.get("sessionId", None)
+    service_code = request.values.get("serviceCode", None)
+    text = request.values.get("text", None)
+    phone_number = request.values.get("phoneNumber", None)
+    if text =="":
+        response = "END hi welcome to your ussd application"
+    # usrResponse = text.split("*")[0]
+
+    # if text == ''
